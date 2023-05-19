f4c5b293c70c2bc3aeabba848c53bcfee5175aaa
Guilherme Couto <gdancouto@gmail.com>
2023-05-19 15:12:33 -0300

    Changing parameters

--- main/scripts/switcher.sh
+++ main/scripts/switcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tsswitch --remote 2222 --fast-switch \
--I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream1.ts -P regulate --bitrate 10000000" \
--I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream2.ts -P regulate --bitrate 10000000" \
--O fork "tsp --realtime -P regulate --bitrate 10653409 -P continuity --fix -O ip 224.0.0.1:4445"
+-I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream1.ts -P regulate\
+-I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream2.ts -P regulate\
+-O fork "tsp --realtime -P regulate -P continuity --fix -O ip 239.1.1.1:4445"
