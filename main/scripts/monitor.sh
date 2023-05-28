1d8e9d3812a464717626ad4e2561bf9f5b0dc2cf
Guilherme Couto <gdancouto@gmail.com>
2023-05-27 22:57:07 -0300

    Changing enviroment

--- main/scripts/monitor.sh
+++ main/scripts/monitor.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-GENOPTIONS="-fflags nobuffer -analyzeduration 0 -re"
+#GENOPTIONS="-fflags nobuffer -analyzeduration 0 -re"
 #VIDSOURCE="-"
-VIDSOURCE="udp://239.1.1.1:4445?overrun_nonfatal=1"
-AUDIO_OPTS=""
-VIDEO_OPTS="-vcodec copy"
-OUTPUT_HLS="-hls_time 2 -hls_list_size 1 -start_number 1 -hls_flags delete_segments"
-OUTPUT_PATH="$(pwd)/../graphics/public"
-OUTPUT_FILE="$OUTPUT_PATH/browser.m3u8"
+#VIDSOURCE="udp://239.1.1.1:4445?overrun_nonfatal=1\&fifo_size=50000000"
+#AUDIO_OPTS=""
+#VIDEO_OPTS="-vcodec copy"
+#OUTPUT_HLS="-hls_time 2 -start_number 1 -hls_flags delete_segments"
+#OUTPUT_PATH="$(pwd)/../graphics/public"
+#OUTPUT_FILE="$OUTPUT_PATH/browser.m3u8"
 
-rm $OUTPUT_PATH/*.ts 
+#rm $OUTPUT_PATH/*.ts 
 #rm $OUTPUT_FILE
 
-#tsp --realtime \
-#-I ip 239.1.1.1:4445 \
-#-P splicemonitor --json-udp 127.0.0.1:4444 \
+tsp --realtime \
+-I ip 239.1.1.1:4445 \
+-P splicemonitor --json-udp 127.0.0.1:4444 \
+-O ip 192.168.1.20:4445
+
 #-O fork "ffmpeg $GENOPTIONS -i $VIDSOURCE -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_FILE"
 
-ffmpeg $GENOPTIONS -i $VIDSOURCE -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_FILE
+#-P fork "tsp -O ip 192.168.1.20:4445" \
+#-O ip 192.168.1.20:4445
+#$ffmpeg $GENOPTIONS -i $VIDSOURCE -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_FILE
--- main/scripts/switcher.sh
+++ main/scripts/switcher.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#tsswitch --remote 2222 --fast-switch \
-#-I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream1.ts -P regulate" \
-#-I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream2.ts -P regulate" \
-#-O fork "tsp --realtime --max-input-packets 100 --max-flushed-packets 100 -P regulate -P continuity --fix -O ip 239.1.1.1:4445"
+tsswitch --remote 2222 --fast-switch \
+-I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream1.ts -P regulate" \
+-I fork "tsp --realtime -I file --infinite $(pwd)/../streams/stream2.ts -P regulate" \
+-O fork "tsp --realtime --max-input-packets 100 --max-flushed-packets 100 -P regulate -P continuity --fix -O ip 239.1.1.1:4445"
 
 
-GENOPTIONS="-re"
-VIDSOURCE="$(pwd)/../streams/stream1.ts"
-AUDIO_OPTS=""
-VIDEO_OPTS="-vcodec copy"
-OUTPUT_HLS=""
-OUTPUT_PATH="$(pwd)/../graphics/public"
-OUTPUT_FILE="-f mpegts udp://239.1.1.1:4445"
+#GENOPTIONS="-re"
+#VIDSOURCE="$(pwd)/../streams/stream1.ts"
+#AUDIO_OPTS=""
+#VIDEO_OPTS="-vcodec copy"
+#OUTPUT_HLS=""
+#OUTPUT_PATH="$(pwd)/../graphics/public"
+#OUTPUT_FILE="-f mpegts udp://239.1.1.1:4445"
 
-ffmpeg $GENOPTIONS -i $VIDSOURCE -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_FILE
+#ffmpeg $GENOPTIONS -i $VIDSOURCE -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_FILE
