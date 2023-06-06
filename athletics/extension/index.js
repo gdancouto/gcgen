a6983ea07fe560e1ffd7d910c81ef51316373a3f
Guilherme Couto <gdancouto@gmail.com>
2023-06-06 01:47:55 -0300

    Improving Interface

--- athletics/extension/index.js
+++ athletics/extension/index.js
@@ -1,12 +1,49 @@
 module.exports = nodecg => {
+}
 	
-	nodecg.listenFor('athleticsChannel', (newValue) => {
+	/*const config = { 
+
+		template:{
+			domain: "GAMES",
+			package: "athletics",  //this package name?
+			graphic: "",
+			channel: "",
+			layer: 0,
+			command: 0,
+		}
+	}
+
+	const channelToSend = {
+		PGM: true,
+		PVW: false,
+	}
+
+	module.exports = nodecg => {
+
+	// LISTEN FOR DASHBOARD --------------------------------------------------
+
+	nodecg.listenFor(config.template.package + "TX", (newValue) => {
 
 		try {
-			nodecg.sendMessageToBundle('mainChannel', 'main', newValue);
+			if (channelToSend[newValue.template.channel])
+				nodecg.sendMessageToBundle('mainChannel', 'main', newValue);
+
+		} catch (error) {
+			nodecg.log.error(error);
+		}
+	});
+
+	// LISTEN FOR MAIN CHANNELS --------------------------------------------------
+
+	nodecg.listenFor(config.template.package + "RX", (newValue) => {
+
+		try {
+			nodecg.sendMessageToBundle(newValue.template.graphic, newValue);
 
 		} catch (error) {
 			nodecg.log.error(error);
 		}
 	});
 }
+
+*/
